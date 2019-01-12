6c9b6ec2b33fa324a8155c465aac544b40088b5a
Bussaki Masahiro <nuubou@gmail.com>
2019-01-12 11:47:15 +0900

    add setupdocker.sh

---
+++ setup_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+sudo service docker restart
